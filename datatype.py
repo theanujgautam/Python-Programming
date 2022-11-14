c5d7d02f07564064f93af407fdfcce0f76e7af88
theanujgautam <61941950+theanujgautam@users.noreply.github.com>
2022-11-15 02:08:32 +0530

    Initial commit

---
+++ datatype.py
@@ -0,0 +1,10 @@
+# Python Datatypes:
+# Numeric - int, long, complex
+# Numeric with decimal point - float
+# Alphnumeric - String
+# Boolean - bool
+
+print(type(3))
+print(type("Hello World"))
+print(type(False))
+print(type(2.0))
---
+++ dynamicTyping.py
@@ -0,0 +1,22 @@
+# Python is a dynamically typed languague, 
+# i.e. the type of variable is dynamically and automatically assigned 
+# as per how the value is used
+
+
+#int
+num1 = 65
+
+#complex
+num2 = 1j   
+
+#str
+message = 'hello world'
+
+#bool
+flag = True
+
+print(type(num1))
+print(type(num2))
+print(type(message))
+print(type(flag))
+
